5c2b61d34a4774403f4c4a3734489122bff3e0c4
Dmitry Vegel <dm.vegel@gmail.com>
2025-10-29 21:20:22 +0300

    upd fix moved from controller to service

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -4,6 +4,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDate;
@@ -40,6 +41,9 @@ public class MealService {
 
     public void update(Meal meal, int userId) {
         Assert.notNull(meal, "meal must not be null");
+        User user = new User();
+        user.setId(userId);
+        meal.setUser(user);
         checkNotFound(repository.save(meal, userId), meal.id());
     }
 
