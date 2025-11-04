96a546eda433951fd257992b9b3020b81231e729
Dmitry Vegel <dm.vegel@gmail.com>
2025-11-04 19:08:55 +0300

    added test for empty meals

--- src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
@@ -9,9 +9,9 @@ import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
+import static java.util.Collections.emptyList;
 import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
-import static ru.javawebinar.topjava.UserTestData.USER_ID;
-import static ru.javawebinar.topjava.UserTestData.USER_MATCHER;
+import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(profiles = Profiles.DATAJPA)
 public class UserServiceDataJpaTest extends AbstractUserServiceTest {
@@ -20,5 +20,12 @@ public class UserServiceDataJpaTest extends AbstractUserServiceTest {
         User result = service.getUserWithMeals(USER_ID);
         USER_MATCHER.assertMatch((User) Hibernate.unproxy(result), UserTestData.user);
         MEAL_MATCHER.assertMatch(result.getMeals(), MealTestData.meals);
+    }
+
+    @Test
+    public void getUserWithEmptyMeals() {
+        User result = service.getUserWithMeals(GUEST_ID);
+        USER_MATCHER.assertMatch((User) Hibernate.unproxy(result), UserTestData.guest);
+        MEAL_MATCHER.assertMatch(result.getMeals(), emptyList());
     }
 }
